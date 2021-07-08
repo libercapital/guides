78a6987acad7b7ea1621702850226f9d6d437b52
José Antonio <juninho.juze@gmail.com>
2021-07-08 09:35:03 -0300

    (+) Add specification for loop variable name

--- ruby/sample.rb
+++ ruby/sample.rb
@@ -1,4 +1,11 @@
 module A
   class B
+    def list_cmyk(index)
+      colors = ['cyan', 'magenta', 'yellow', 'key']
+
+      colors.each_with_index do |color, index|
+        puts "Color #{index} is #{color}"
+      end
+    end
   end
 end
