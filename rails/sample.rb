e00478e3cc8bf67a74c48f016e05e00c0f71eb63
José Antonio <juninho.juze@gmail.com>
2021-07-26 16:08:01 -0300

    (+) Joins vs Includes docs

---
+++ rails/sample.rb
@@ -0,0 +1,20 @@
+# bad
+users = User.joins(:company).where(status: :active)
+users.each do |user|
+  puts "Name: #{user.name}"
+  puts "Company: #{user.company.name}" # to retrieve user.company.name, each time a call to database is made
+end
+
+# good
+users = User.includes(:company).where(status: :active)
+users.each do |user|
+  puts "Name: #{user.name}"
+  puts "Company: #{user.company.name}" # the user.company.name is already loaded
+end
+
+# good
+users = User.joins(:company).where(companies: { status: :onboarded })
+users.each do |user|
+  puts "Name: #{user.name}"
+  puts "Address: #{user.address}"
+end
